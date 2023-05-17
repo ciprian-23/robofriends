eb5e3e2dafeca4610c8bd4bb7ea05925f9fcdb97
Ciprian <ciprian.lera@gmail.com>
2023-05-17 11:40:47 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+// import { robots } from './Robots';
 import './App.css';
+import Scroll from './Scroll';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: '',
+    };
+  }
+
+  componentDidMount() {
+    // console.log('check');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    // console.log(event.target.value);
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    if (robots.length === 0) {
+      return <h1>Loading robots...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">Robo Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Ghita Popescu',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+  },
+  {
+    id: 2,
+    name: 'Marin Ionescu',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+  },
+  {
+    id: 3,
+    name: 'Maria Alecu',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+  },
+  {
+    id: 4,
+    name: 'Ion Popescu',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+  },
+  {
+    id: 5,
+    name: 'Vasile Avramescu',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca',
+  },
+  {
+    id: 6,
+    name: 'Paul Georgescu',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info',
+  },
+  {
+    id: 7,
+    name: 'Ion Vasilescu',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz',
+  },
+  {
+    id: 8,
+    name: 'Andreea Maxim',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me',
+  },
+  {
+    id: 9,
+    name: 'Iulia Zugravu',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io',
+  },
+  {
+    id: 10,
+    name: 'Alina Paulescu',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz',
+  },
+];
